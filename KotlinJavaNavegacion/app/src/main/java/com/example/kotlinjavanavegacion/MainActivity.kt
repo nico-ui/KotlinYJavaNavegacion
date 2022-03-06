31b0433b53d69cdd71156e6617fb2e4a4c2892a0
cristiannicolai.hernandez.contractor <cristiannicolai.hernandez.contractor@bbva.com>
2022-03-05 20:46:54 -0600

    Configuracion basica

---
+++ KotlinJavaNavegacion/app/src/main/java/com/example/kotlinjavanavegacion/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.kotlinjavanavegacion
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun clickGo(view: View){
+        val text = "click boton GO"
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(applicationContext, text, duration)
+        toast.show()
+        val downloadIntent = Intent(this, MainActivity2::class.java)
+        startService(downloadIntent)
+    }
+
+}
---
+++ KotlinJavaNavegacion/app/src/main/java/com/example/kotlinjavanavegacion/MainActivity2.java
@@ -0,0 +1,32 @@
+package com.example.kotlinjavanavegacion;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        btnBack = findViewById(R.id.btnBack);
+    }
+
+    public void clickBack(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "click boton back";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        Intent downloadIntent = new Intent(this, MainActivity.class);
+        startService(downloadIntent);
+    }
+
+}
